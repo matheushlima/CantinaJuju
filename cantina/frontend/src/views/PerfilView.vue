4bb5ac793de0f0efbf307dbf89cba99acf03b4d4
BrunoRicardoCJ <brunoricardocj@gmail.com>
2023-08-09 14:06:07 -0300

    perfil

--- cantina/frontend/src/views/PerfilView.vue
+++ cantina/frontend/src/views/PerfilView.vue
@@ -2,20 +2,17 @@
     <NavBar/>
     <atalho-list-aluno/>
     <cantinas-list/>
-    <popular-items-container/>
 </template>
 
 <script>
 import NavBar from '../components/app-cantina/NavBar.vue';
 import AtalhoListAluno from '@/components/app-cantina/AtalhoListAluno.vue';
 import CantinasList from '@/components/app-cantina/CantinaLista.vue';
-import PopularItem from '@/components/app-cantina/PopulerItems.vue';
 
 export default {
   components: { NavBar,
     AtalhoListAluno,
-    CantinasList,
-    PopularItem },
+    CantinasList },
 }
 </script>
 
